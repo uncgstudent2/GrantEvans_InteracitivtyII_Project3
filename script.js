839e243881ecd828e31722103e1e2edd5d701a22
grant <uncgstudent64>
2026-04-27 22:44:05 -0400

    final done I'm tired

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let imgArr=["https://npr.brightspotcdn.com/dims4/default/d0fefd8/2147483647/strip/true/crop/2560x1707+0+0/resize/880x587!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2F67%2F7c%2F6165e3d642faa8b5ecc33aa92e2c%2Fdsc09529.jpg","https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638887501/EducationHub/photos/racetrack-playa.jpg"]
+let imgArr=["https://npr.brightspotcdn.com/dims4/default/d0fefd8/2147483647/strip/true/crop/2560x1707+0+0/resize/880x587!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2F67%2F7c%2F6165e3d642faa8b5ecc33aa92e2c%2Fdsc09529.jpg","https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638887501/EducationHub/photos/racetrack-playa.jpg","https://images.pexels.com/photos/416528/pexels-photo-416528.jpeg","https://media.licdn.com/dms/image/v2/D5612AQFnICnWV6PhQg/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1681458630459?e=2147483647&v=beta&t=v_2WL8CcRb0YIiUL8RCep0hbwiak4PJMxCTAmRX0Yfw","https://thumb.ac-illust.com/a5/a559a598ab8815943b98635d102ffff2_t.jpeg","https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2xhcmdlc3QtZGVzZXJ0LXdvcmxkLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6IjEyMDAifX19","https://www.telegraph.co.uk/content/dam/Travel/2017/October/wildlife-cheetah-namibia-GETTY.jpg?imwidth=640","https://image.pbs.org/bento3-prod/changingseas-tv2/Season%2012/1203/1203%20Photo%20Gallery%203rd%20UPload/a10d530063_1203-8.jpg","https://features.boats.com/boat-content/files/2015/06/boating-with-dogs-onboard-boats.jpg","https://images2.minutemediacdn.com/image/upload/c_crop,x_0,y_0,w_1279,h_719/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/images/voltaxMediaLibrary/mmsport/fishing_on_si/01jyw51x4n84yj1m7ym1.jpg","https://torchcraftmarine.com/wp-content/uploads/sb-instagram-feed-images/670175625_18431830009185915_800352099756660339_nfull.webp","https://www.westmarine.com/on/demandware.static/-/Sites-WestMarine-Library/default/dw26b491a9/Bring-Your-Dog-Aboard.jpg","https://features.boats.com/boat-content/files/2015/06/boating-with-dogs-onboard-boats.jpg","https://images2.minutemediacdn.com/image/upload/c_crop,x_0,y_0,w_1279,h_719/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/images/voltaxMediaLibrary/mmsport/fishing_on_si/01jyw51x4n84yj1m7ym1.jpg","https://torchcraftmarine.com/wp-content/uploads/sb-instagram-feed-images/670175625_18431830009185915_800352099756660339_nfull.webp","https://www.westmarine.com/on/demandware.static/-/Sites-WestMarine-Library/default/dw26b491a9/Bring-Your-Dog-Aboard.jpg"]
+
+let message=["We who drank all the water and now have nothing left to drink","We who drank all the water and now have nowhere left to sail.","We miss you.","We who now walk under the scorching sun among shipwrecks.","We who now finally feel the smell of your rotting body.","We bury you","We who will soon diebecause we have been so thirsty.","We whose tears can fill rivers seas oceans.","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail","We Sail"]
+// a= "https://npr.brightspotcdn.com/dims4/default/d0fefd8/2147483647/strip/true/crop/2560x1707+0+0/resize/880x587!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2F67%2F7c%2F6165e3d642faa8b5ecc33aa92e2c%2Fdsc09529.jpg","https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638887501/EducationHub/photos/racetrack-playa.jpg"
+//[a,b,c,d]
+//[a,1,10,50]
 let imgIndex=0;
+let messageIndex=0;
 
 const button = document.getElementById('thisButton');
 
 function clickThisButton(){
-    document.getElementById('message').innerText = "button was clicked!";
-    alert("hello guys!")
+    document.getElementById('message').innerText = "Why waste the water?";
+    alert("TO BAD!")
 }
 
 function changeImage(){
-    document.getElementById('imgChange').src=imgArr[imgIndex];
+    document.getElementById('imgChange').src=imgArr[imgIndex];//finding the element tha that has an id of 'imgChange'
+    document.getElementById('message').innerText=message[messageIndex]
+    //find its 'src' attributes for<img> 
+    // replace it with an item in imgArr
+    //that item is decided by imgIndex
     imgIndex+=1;
     if(imgIndex>imgArr.length-1){
         imgIndex=0;
     }
     console.log("image changed");
+    messageIndex+=1;
+    if(messageIndex>message.length-1){
+        messageIndex=0;
+    }
 }
